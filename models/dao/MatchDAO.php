1a7b0bfff0d121b383fdd3829f35364b83c467b5
thi-cr <th.cravatte@outlook.com>
2021-06-14 18:48:27 +0200

    :)

--- models/dao/MatchDAO.php
+++ models/dao/MatchDAO.php
@@ -38,4 +38,69 @@ class MatchDAO extends AbstractDAO
         );
     }
 
+
+    function store($id, $data)
+    {
+        if (empty($data['equipe1']) || empty($data['equipe2']) || empty($data['ScoreEquipe1']) || empty($data['ScoreEquipe2']) || empty($data['jeu']) || empty($data['statut']) || empty($data['idCreateur'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare(
+                "INSERT INTO {$this->table} (equipe1, equipe2, , pseudo, email) VALUES (?, ?, ?, ?, ?)"
+            );
+            $statement->execute([
+                htmlspecialchars($data['equipe1']),
+                htmlspecialchars($data['equipe2']),
+                htmlspecialchars($data['ScoreEquipe1']),
+                htmlspecialchars($data['ScoreEquipe2']),
+                htmlspecialchars($data['jeu']),
+                htmlspecialchars($data['statut']),
+                htmlspecialchars($data['idCreateur']),
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
+
+
+    function delete($data)
+    {
+        if (empty($data['id'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare("DELETE FROM {$this->table} WHERE id = ?");
+            $statement->execute([
+                $data['id']
+            ]);
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+
+    public function update($id, $data)
+    {
+        try {
+            $statement = $this->connection->prepare("UPDATE {$this->table} SET equipe1 = ?, equipe2 = ?, ScoreEquipe1 = ?, ScoreEquipe2 = ?, jeu = ?, statut, idCreateur = ? WHERE id = ?");
+            $statement->execute(
+                [
+                    htmlspecialchars($data['equipe1']),
+                    htmlspecialchars($data['equipe2']),
+                    htmlspecialchars($data['ScoreEquipe1']),
+                    htmlspecialchars($data['ScoreEquipe2']),
+                    htmlspecialchars($data['jeu']),
+                    htmlspecialchars($data['statut']),
+                    htmlspecialchars($data['idCreateur']),
+                    htmlspecialchars($data['id'])
+                ]
+            );
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
+
 }
