c06ed9aa11c584eace68249c596ab89b0713701b
thi-cr <th.cravatte@outlook.com>
2021-06-16 14:27:35 +0200

    :DDDDDDDD

--- views/joueur/logged.php
+++ views/joueur/logged.php
@@ -1,14 +1,16 @@
 <?php var_dump($joueur); ?>
 <?php var_dump($joueur->equipe->id); ?>
-<h3>créer un match</h3>
-<form action="/matchs/add">
-    <select name="equipe" id="equipe">
-        <?php foreach ($equipes as $equipe): ?>
-            <?php if ($equipe->id != $joueur->equipe->id): ?>
-                <?php var_dump($equipes); ?>
-                <option type="number" name="equipe_id" value="<?= $equipe->id ?>"><?= $equipe->nom ?></option>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </select>
-    <input type="submit" value="Créer un match contre l'equipe sélectionnée">
-</form>
+<?php if ($joueur->id == $joueur->equipe->capitaine_id): ?>
+    <h3>créer un match</h3>
+    <form action="/matchs/add">
+        <select name="equipe" id="equipe">
+            <?php foreach ($equipes as $equipe): ?>
+                <?php if ($equipe->id != $joueur->equipe->id): ?>
+                    <?php var_dump($equipes); ?>
+                    <option type="number" name="equipe_id" value="<?= $equipe->id ?>"><?= $equipe->nom ?></option>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </select>
+        <input type="submit" value="Créer un match contre l'equipe sélectionnée">
+    </form>
+<?php endif; ?>
