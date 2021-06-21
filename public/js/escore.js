ad913a637027260eb2f6209c0bd7bc780daf1bea
thi-cr <th.cravatte@outlook.com>
2021-06-21 19:40:59 +0200

    JS select multiple

--- public/js/escore.js
+++ public/js/escore.js
@@ -33,5 +33,18 @@ $(document).ready(function() {
     })
 
 
+    $("select").mousedown(function(e){
+        e.preventDefault();
+
+        var select = this;
+        var scroll = select.scrollTop;
+
+        e.target.selected = !e.target.selected;
+
+        setTimeout(function(){select.scrollTop = scroll;}, 0);
+
+        $(select).focus();
+    }).mousemove(function(e){e.preventDefault()});
+
 
 });
